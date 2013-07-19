d4a4c3dcc83f902b0ebd60ba7a2f2649dc2f2468
Sergey Zhigalov <sergey.zhigalov@gmail.com>
2013-07-19 12:10:45 +0600

    Обновил Redis и реализовал сохранение базы на диск

--- Factory/OptimusPrimeFactory/OptimusPrimeFactory.cs
+++ Factory/OptimusPrimeFactory/OptimusPrimeFactory.cs
@@ -1,4 +1,7 @@
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using BookSleeve;
@@ -9,6 +12,7 @@ namespace OptimusPrime.Factory
     public partial class OptimusPrimeFactory : IFactory
     {
         private readonly List<Thread> threads;
+        private RedisConnection connection;
         private static IList<IOptimusPrimeService> Services { get; set; }
 
         public OptimusPrimeFactory()
@@ -19,7 +23,7 @@ namespace OptimusPrime.Factory
 
         public void Start()
         {
-            var connection = new RedisConnection("localhost", allowAdmin: true);
+            connection = new RedisConnection("localhost", allowAdmin: true);
 
             Task openTask = connection.Open();
             connection.Wait(openTask);
@@ -39,6 +43,21 @@ namespace OptimusPrime.Factory
         {
             foreach (var thread in threads)
                 thread.Abort();
+
+            var fileName = "C:\\" +
+                           String.Format("{0:yyyy.MM.dd_hh.mm.ss.fff}", DateTime.Now) + ".opdump";
+            var fileStream = new FileStream(fileName, FileMode.CreateNew, FileAccess.Write);
+
+            foreach (var service in Services)
+                foreach (var output in service.OptimusPrimeOut)
+                {
+                    var exportTask = connection.Server.Export(output.Service.DbPage, output.Name);
+                    var value = connection.Wait(exportTask);
+
+                    fileStream.Write(value, 0, value.Length);
+                    fileStream.Flush();
+                }
+            fileStream.Close();
         }
     }
 }
