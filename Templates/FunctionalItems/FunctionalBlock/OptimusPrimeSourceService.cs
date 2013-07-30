7aedb10c992e9f423f9df826f2a8270de292eeb3
Sergey Zhigalov <sergey.zhigalov@gmail.com>
2013-07-30 16:25:48 +0600

    Подписку на событие от SourceBlock в OptimusPrimeSourceService выполняю в кострукторе, чтобы при старте системы вне зависимости от порядка старта сервисов у SourceBlock всегда были подписчики close #36

--- Templates/FunctionalItems/FunctionalBlock/OptimusPrimeSourceService.cs
+++ Templates/FunctionalItems/FunctionalBlock/OptimusPrimeSourceService.cs
@@ -4,19 +4,17 @@ namespace OptimusPrime.Templates
 {
     public class OptimusPrimeSourceService<TData> : OptimusPrimeService
     {
-        private readonly ISourceBlock<TData> sourceBlock;
         public IOptimusPrimeOut Output { get { return OptimusPrimeOut[0]; } }
 
         public OptimusPrimeSourceService(ISourceBlock<TData> sourceBlock, string outputName)
         {
-            this.sourceBlock = sourceBlock;
             OptimusPrimeOut = new IOptimusPrimeOut[] {new OptimusPrimeOut(outputName, this)};
             OptimusPrimeIn = new IOptimusPrimeIn[0];
+            sourceBlock.Event += (sender, e) => Output.Set(e);
         }
 
         public override void Actuation()
         {
-            sourceBlock.Event += (sender, e) => Output.Set(e);
         }
     }
 }
