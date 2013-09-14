b7bed4ba7ddee0494b543357fd4e1bcd3932a3ef
okulovsky <yuri.okulovsky@gmail.com>
2013-09-14 14:59:40 +0600

    Добавил кодогенерацию для SourceCollector.cs

--- Factory/GenericFactory/SourceCollector/Codegen.cs
+++ Factory/GenericFactory/SourceCollector/Codegen.cs
@@ -1,4 +1,5 @@
-﻿using OptimusPrime.Templates;
+﻿
+using OptimusPrime.Templates;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,27 +8,67 @@ using System.Threading.Tasks;
 
 namespace OptimusPrime.Factory
 {
+
     partial class IFactoryExtensions
     {
-        public static SourceCollectorHelper<T1, T2> BindSources<T1, T2>(this IFactory factory, ISource<T1> source1, ISource<T2> source2)
+
+
+        public static SourceCollectorHelper<T0> BindSources<T0>(this IFactory factory, ISource<T0> source0)
         {
-            return new SourceCollectorHelper<T1, T2>
+            return new SourceCollectorHelper<T0>
             {
                 readers = new ISourceReader[]
-                {
-                    source1.CreateReader(),
-                    source2.CreateReader()
-                }
+            {
+                source0.CreateReader()
+            }
+            };
+        }
+
+        public static SourceCollectorHelper<T0, T1> BindSources<T0, T1>(this IFactory factory, ISource<T0> source0, ISource<T1> source1)
+        {
+            return new SourceCollectorHelper<T0, T1>
+            {
+                readers = new ISourceReader[]
+            {
+                source0.CreateReader(),source1.CreateReader()
+            }
             };
         }
     }
 
-    public class SourceCollectorHelper<T1, T2> : SourceCollectorHelper
+
+
+    public class SourceCollectorHelper<T0> : SourceCollectorHelper
     {
-        public SourceCollector<Q> CreateCollector<Q>()
-            where Q : SourceDataCollection<T1, T2>, new()
+        public SourceCollector<TOutput> CreateCollector<TOutput>()
+            where TOutput : SourceDataCollection<T0>, new()
         {
-            return new SourceCollector<Q>(readers);
+            return new SourceCollector<TOutput>(readers);
+        }
+    }
+
+    public class SourceCollectorHelper<T0, T1> : SourceCollectorHelper
+    {
+        public SourceCollector<TOutput> CreateCollector<TOutput>()
+            where TOutput : SourceDataCollection<T0, T1>, new()
+        {
+            return new SourceCollector<TOutput>(readers);
+        }
+    }
+
+
+
+    public class SourceDataCollection<T0> : ISourceDataCollection
+    {
+        protected List<T0> List0;
+
+        public int ListCount
+        {
+            get { return 1; }
+        }
+        public void Pull(int index, System.Collections.IEnumerable source)
+        {
+            if (index == 0) { if (List0 == null) List0 = new List<T0>(); List0.AddRange(source.Cast<T0>()); }
         }
     }
 
@@ -35,23 +76,15 @@ namespace OptimusPrime.Factory
     {
         protected List<T0> List0;
         protected List<T1> List1;
+
         public int ListCount
         {
             get { return 2; }
         }
         public void Pull(int index, System.Collections.IEnumerable source)
         {
-            if (index == 0)
-            {
-                if (List0 == null) List0 = new List<T0>();
-                List0.AddRange(source.Cast<T0>());
-            }
-            if (index == 1)
-            {
-                if (List1 == null) List1 = new List<T1>();
-                List1.AddRange(source.Cast<T1>());
-            }
-
+            if (index == 0) { if (List0 == null) List0 = new List<T0>(); List0.AddRange(source.Cast<T0>()); }
+            if (index == 1) { if (List1 == null) List1 = new List<T1>(); List1.AddRange(source.Cast<T1>()); }
         }
     }
 }
