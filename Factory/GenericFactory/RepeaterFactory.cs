5289f0c323d76de5ffe23cd8648bd6655ade3f7b
okulovsky <yuri.okulovsky@gmail.com>
2013-10-30 16:03:04 +0500

    Добавил Repeater, который не требует данных от сенсоров (нужен для развертки коллекции)

--- Factory/GenericFactory/RepeaterFactory.cs
+++ Factory/GenericFactory/RepeaterFactory.cs
@@ -25,5 +25,27 @@ namespace OptimusPrime.Factory
                 return repeaterBlock.Conclude();
             }));
         }
+
+
+        public static IChain<TRepeaterBigIn, TRepeaterBigOut> CreateRepeater
+           <TRepeaterBigIn, TRepeaterBigOut, TChainSmallIn, TChainSmallOut>(this IFactory factory,
+                 IRepeaterBlock<TRepeaterBigIn, TRepeaterBigOut, TChainSmallIn, TChainSmallOut> repeaterBlock,
+             IChain<TChainSmallIn, TChainSmallOut> privateChaine)
+        {
+            return factory.CreateChain(new FunctionalBlock<TRepeaterBigIn, TRepeaterBigOut>(
+             input =>
+             {
+                 var functionalBlock = privateChaine.ToFunctionalBlock();
+                 repeaterBlock.Start(input);
+                 TChainSmallIn smallIn;
+                 TChainSmallOut smallOut = default(TChainSmallOut);
+
+
+                 while (repeaterBlock.MakeIteration(smallOut, out smallIn))
+                     smallOut = functionalBlock.Process(smallIn);
+
+                 return repeaterBlock.Conclude();
+             }));
+        }
     }
 }
--- Templates/FunctionalItems/IRepeaterBlock.cs
+++ Templates/FunctionalItems/IRepeaterBlock.cs
@@ -4,7 +4,13 @@ namespace OptimusPrime.Templates
     {
         void Start(TRepeaterBigIn publicIn);
         bool MakeIteration(TSourceDataCollection sourceDatas, TChainSmallOut oldPrivateOut, out TChainSmallIn privateIn);
-        //todo: не передавть TPrivateOut
+         TRepeaterBigOut Conclude();
+    }
+
+    public interface IRepeaterBlock<TRepeaterBigIn, TRepeaterBigOut, TChainSmallIn, TChainSmallOut>
+    {
+        void Start(TRepeaterBigIn publicIn);
+        bool MakeIteration(TChainSmallOut oldPrivateOut, out TChainSmallIn privateIn);
         TRepeaterBigOut Conclude();
     }
 }
