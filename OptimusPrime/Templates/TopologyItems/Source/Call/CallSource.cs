c6fb17bfd8997ae87bc34ca882e95d1cbcf78aef
sht3ch <shtechgen@gmail.com>
2013-11-12 21:46:56 +0600

    Попытка восстановить цепь коммитов.

--- OptimusPrime/Templates/TopologyItems/Source/Call/CallSource.cs
+++ OptimusPrime/Templates/TopologyItems/Source/Call/CallSource.cs
@@ -1,3 +1,40 @@
+<<<<<<< master
+﻿using System.Collections.Generic;
+using OptimusPrime.Factory;
+
+namespace OptimusPrime.Templates
+{
+    public class CallSource<T> : ICallSource<T>
+    {
+        public IFactory Factory { get; private set; }
+        private readonly IList<SourceReader<T>> _sourceReaders;
+
+        public CallSource(CallFactory factory, string name)
+        {
+            Factory = factory;
+            Name = name;
+            Collection = new List<object>();
+            _sourceReaders = new List<SourceReader<T>>();
+        }
+
+        public ISourceReader<T> CreateReader()
+        {
+            var sourceReader = new SourceReader<T>(this);
+            _sourceReaders.Add(sourceReader);
+            return sourceReader;
+        }
+
+        public string Name { get; private set; }
+
+        public IList<object> Collection { get; private set; }
+
+        public void Release()
+        {
+            foreach (var sourceReader in _sourceReaders)
+                sourceReader.AvailableData.Release();
+        }
+    }
+=======
 ﻿using System.Collections.Generic;
 using OptimusPrime.Factory;
 
@@ -39,4 +76,5 @@ namespace OptimusPrime.Templates
             }
         }
     }
+>>>>>>> local
 }
