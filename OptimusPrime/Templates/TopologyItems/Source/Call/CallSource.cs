c1b9cd5394d5a09db612b9401038e25983dcfdd0
sht3ch <shtechgen@gmail.com>
2013-11-12 22:07:29 +0600

    Работа с контроллером налажена. Из инициализации выпилены праймы. После борьбы с гитом я мог потерять что-то полезное, так что может сразу не завестись. Но компилится точно.

--- OptimusPrime/Templates/TopologyItems/Source/Call/CallSource.cs
+++ OptimusPrime/Templates/TopologyItems/Source/Call/CallSource.cs
@@ -1,4 +1,3 @@
-<<<<<<< master
 ﻿using System.Collections.Generic;
 using OptimusPrime.Factory;
 
@@ -34,47 +33,4 @@ namespace OptimusPrime.Templates
                 sourceReader.AvailableData.Release();
         }
     }
-=======
-﻿using System.Collections.Generic;
-using OptimusPrime.Factory;
-
-namespace OptimusPrime.Templates
-{
-    public class CallSource<T> : ICallSource<T>
-    {
-        public IFactory Factory { get; private set; }
-        private readonly IList<SourceReader<T>> _sourceReaders;
-
-        public CallSource(CallFactory factory, string name)
-        {
-            Factory = factory;
-            Name = name;
-            Collection = new List<object>();
-            _sourceReaders = new List<SourceReader<T>>();
-        }
-
-        public ISourceReader<T> CreateReader()
-        {
-            lock (_sourceReaders)
-            {
-                var sourceReader = new SourceReader<T>(this);
-                _sourceReaders.Add(sourceReader);
-                return sourceReader;
-            }
-        }
-
-        public string Name { get; private set; }
-
-        public IList<object> Collection { get; private set; }
-
-        public void Release()
-        {
-            lock (_sourceReaders)
-            {
-                foreach (var sourceReader in _sourceReaders)
-                    sourceReader.AvailableData.Release();
-            }
-        }
-    }
->>>>>>> local
 }
