a5922d5ee79c47076d2122a5021460fa4406a712
okulovsky <yuri.okulovsky@gmail.com>
2013-11-17 22:17:51 +0500

    Запилил форки и тесты к ним

--- OptimusPrime/Factory/Amenities.cs
+++ OptimusPrime/Factory/Amenities.cs
@@ -50,5 +50,13 @@ namespace OptimusPrime.Factory
         {
             return source.Where(predicate.Process, pseudoName);
         }
+
+        public static Fork<TIn, TOut> Fork<TIn, TOut>(this IChain<TIn, TOut> chain)
+        {
+            var forkBlock = new ForkBlock<TIn, TOut>(new Func<TIn, TOut>(chain.ToFunctionalBlock().Process));
+            return new Fork<TIn, TOut>(
+                chain.Factory.CreateChain(forkBlock.ForkedAction),
+                chain.Factory.CreateSource(forkBlock.Source));
+        }
     } 
 }
