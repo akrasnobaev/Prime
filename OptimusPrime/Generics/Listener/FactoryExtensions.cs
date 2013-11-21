29c35baf5308151c66472a5f50700a05d065da2b
okulovsky <yuri.okulovsky@gmail.com>
2013-11-21 14:25:00 +0600

    Добавил офигенно полезый сервис Listener

---
+++ OptimusPrime/Generics/Listener/FactoryExtensions.cs
@@ -0,0 +1,21 @@
+﻿using OptimusPrime.Generics;
+using OptimusPrime.Templates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OptimusPrime.Factory
+{
+    public static partial class FactoryExtensions
+    {
+        public static void Listen<TInput>
+            (this ISource<TInput> input, Action<TInput> process)
+        {
+            var service = new ListenerService<TInput>(input, process);
+            input.Factory.RegisterGenericService(service);
+        }
+
+       
+    }
+}
---
+++ OptimusPrime/Generics/Listener/ListenerService.cs
@@ -0,0 +1,31 @@
+﻿using OptimusPrime.Templates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OptimusPrime.Generics
+{
+    public class ListenerService<T> : IGenericService 
+    {
+        ISource<T> input;
+        ISourceReader<T> reader;
+        Action<T> action;
+        public ListenerService(ISource<T> input, Action<T> action)
+        {
+            this.input = input;
+            this.action = action;
+        }
+
+        public void Initialize()
+        {
+            reader = input.CreateReader();
+        }
+
+        public void DoWork()
+        {
+            while (true)
+                action(reader.Get());
+        }
+    }
+}
