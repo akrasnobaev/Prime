aa37f73923eb89b658d5efeb0783e9bfaca0d6fd
okulovsky <yuri.okulovsky@gmail.com>
2013-11-21 15:36:02 +0600

    Тест заработал

--- OptimusPrimeTest/Generics/EnumerableLinkTest.cs
+++ OptimusPrimeTest/Generics/EnumerableLinkTest.cs
@@ -17,11 +17,13 @@ namespace OptimusPrimeTests.Generics
 
             var producer = new DataProducer<int>();
             var source = factory.CreateSource(producer.SourceBlock);
-            var source1 = source.LinkToEnumerable(z => Enumerable.Range(0,OneToMany).Select(x=>z));
+            var source1 = source.LinkToEnumerable(
+                z => Enumerable.Range(0,OneToMany).Select(x=>z)
+                );
             var array = new List<int>();
             source1.Listen(z => { array.Add(z); });
             factory.Start();
-            producer.Start(count, 0, z=>z);
+            producer.Start(count, 3, z=>z);
             factory.Stop();
             Assert.AreEqual(count*OneToMany, array.Count);
             for (int i = 0; i < array.Count; i++)
