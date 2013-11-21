d43ee0bce204f606e26a60a51fdca1e35f00f9bb
okulovsky <yuri.okulovsky@gmail.com>
2013-11-21 14:32:20 +0600

    Добавил тест на слушателя, все работает

---
+++ OptimusPrimeTest/CommonClasses.cs
@@ -0,0 +1,22 @@
+﻿using Eurobot.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace OptimusPrimeTests
+{
+    public class DataProducer<T>
+    {
+        public readonly SourceBlock<T> SourceBlock = new SourceBlock<T>();
+        public void Start(int count, int rate, Func<int, T> data)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                SourceBlock.Publish(data(i));
+                Thread.Sleep(rate);
+            }
+        }
+    }
+}
