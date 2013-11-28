85f193daf8041eac623e87b00a8d768efd95c72b
okulovsky <yuri.okulovsky@gmail.com>
2013-11-28 20:36:47 +0600

    Добавил изоляцию цепей. Теперь одну и ту же цепь можно использовать дважды.
    Поправил баги с неймспейсами в тестах.

---
+++ OptimusPrime/Generics/Isolator/ChainExtension.cs
@@ -0,0 +1,16 @@
+﻿using OptimusPrime.Templates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OptimusPrime
+{
+    public static partial class ChainExtension
+    {
+        public static Isolator<TIn, TOut> Isolate<TIn, TOut>(this IChain<TIn, TOut> chain)
+        {
+            return new Isolator<TIn, TOut>(chain);
+        }
+    }
+}
---
+++ OptimusPrime/Generics/Isolator/Isolator.cs
@@ -0,0 +1,34 @@
+﻿using OptimusPrime.Templates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OptimusPrime
+{
+    public class Isolator<TIn,TOut>
+    {
+        IChain<TIn, TOut> isolatedChain;
+        IFunctionalBlock<TIn, TOut> funcBlock;
+        public Isolator(IChain<TIn, TOut> isolated)
+        {
+            this.isolatedChain = isolated;
+            this.funcBlock = isolatedChain.ToFunctionalBlock();
+        }
+        object lockObject = new object();
+        public IChain<TIn, TOut> CreateIsolated()
+        {
+            return isolatedChain.Factory.CreateChain(
+                new Func<TIn, TOut>(
+                    z =>
+                    {
+                        TOut result = default(TOut);
+                        lock (lockObject)
+                        {
+                            result = funcBlock.Process(z);
+                        }
+                        return result;
+                    }));
+        }
+    }
+}
