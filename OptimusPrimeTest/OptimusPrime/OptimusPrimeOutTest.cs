5f32186101bbd10dcdcb26cf801e4d1a95e3642e
Sergey Zhigalov <sergey.zhigalov@gmail.com>
2014-01-19 17:59:50 +0600

    Тесты на логирование TimeStamp в OptimusOut

--- OptimusPrimeTest/OptimusPrime/OptimusPrimeOutTest.cs
+++ OptimusPrimeTest/OptimusPrime/OptimusPrimeOutTest.cs
@@ -1,40 +1,92 @@
+using System;
 using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using BookSleeve;
 using NUnit.Framework;
+using Prime;
 using Prime.Optimus;
 
 namespace OptimusPrimeTest.Prime
 {
     [TestFixture]
-    // TODO: test time stamps
     public class OptimusPrimeOutTest
     {
-        private OptimusIn _optimusIn;
-        private OptimusOut _optimusOut;
+        private OptimusOut optimusOut;
+        private OptimusStabService stabService;
+        private Stopwatch stopwatch;
         private const string storageKey = "storageKey";
         private const int DataCount = 3;
 
         [SetUp]
         public void Setup()
         {
-            var stabService = new OptimusStabService(dbPage: 2);
-            _optimusIn = new OptimusIn(storageKey, stabService);
-            _optimusOut = new OptimusOut(storageKey, stabService, new Stopwatch());
+            stabService = new OptimusStabService(dbPage: 2);
+            stopwatch = new Stopwatch();
+            optimusOut = new OptimusOut(storageKey, stabService, stopwatch);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            var connection = new RedisConnection("localhost", allowAdmin: true);
+
+            Task openTask = connection.Open();
+            connection.Wait(openTask);
+
+            Task flushDbTask = connection.Server.FlushAll();
+            connection.Wait(flushDbTask);
         }
 
         [Test]
         public void TestSet()
         {
+            var optimusIn = new OptimusIn(storageKey, stabService);
             TestData testData;
+            Assert.IsFalse(optimusIn.TryGet(out testData));
+
             var datas = TestData.CreateData(DataCount);
-
-            Assert.IsFalse(_optimusIn.TryGet(out testData));
             foreach (var data in datas)
-                _optimusOut.Set(data);
+                optimusOut.Set(data);
 
             foreach (var data in datas)
-                _optimusIn.Get<TestData>().AssertAreEqual(data);
+                optimusIn.Get<TestData>().AssertAreEqual(data);
 
-            Assert.IsFalse(_optimusIn.TryGet(out testData));
+            Assert.IsFalse(optimusIn.TryGet(out testData));
+        }
+
+        [Test]
+        public void TestTimeStamps()
+        {
+            var timeStampKey = ServiceNameHelper.GetTimeStampName(storageKey);
+            TimeSpan result;
+            var emptyRange = stabService.Connection.Lists.Range(stabService.DbPage, timeStampKey, 0, -1);
+            var emptyBytes = stabService.Connection.Wait(emptyRange);
+            var timeStamps = emptyBytes.Select(SerializeExtension.Deserialize<TimeSpan>);
+            CollectionAssert.IsEmpty(timeStamps);
+
+            var datas = TestData.CreateData(DataCount);
+            const int waitTime = 5;
+            stopwatch.Start();
+            foreach (var data in datas)
+            {
+                Thread.Sleep(waitTime);
+                optimusOut.Set(data);
+            }
+
+            var range = stabService.Connection.Lists.Range(stabService.DbPage, timeStampKey, 0, -1);
+            var bytes = stabService.Connection.Wait(range);
+            var timeStampList = bytes.Select(SerializeExtension.Deserialize<TimeSpan>).ToArray();
+            Assert.AreEqual(DataCount, timeStampList.Length);
+
+            var expectedTimeStamp = 0;
+            for (int i = 0; i < DataCount; i++)
+            {
+                expectedTimeStamp += waitTime;
+                Debug.WriteLine(timeStampList[i]);
+                Assert.Less(expectedTimeStamp, timeStampList[i].TotalMilliseconds);
+            }
         }
     }
 }
